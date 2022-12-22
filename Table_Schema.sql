372e9e9a84614138c246c254ea6a9d92fd634d65
TonyHHNg <tonynghh@gmail.com>
2022-12-22 14:57:11 +0000

    final

--- Table_Schema.sql
+++ Table_Schema.sql
@@ -1,7 +1,32 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- Link to schema: https://app.quickdatabasediagrams.com/#/d/BChhtx
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
+CREATE TABLE "employees" (
+    "emp_no" int   NOT NULL,
+    "emp_title_id" varchar(10)   NOT NULL,
+    "birth_date" date   NOT NULL,
+    "first_name" varchar(40)   NOT NULL,
+    "last_name" varchar(40)   NOT NULL,
+    "sex" varchar(1)   NOT NULL,
+    "hire_date" date   NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "titles" (
+    "title_id" varchar(10)   NOT NULL,
+    "title" varchar(40)   NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" int   NOT NULL,
+    "salary" int   NOT NULL
+);
 
 CREATE TABLE "departments" (
     "dept_no" varchar(10)   NOT NULL,
@@ -21,31 +46,6 @@ CREATE TABLE "dept_manager" (
     "emp_no" int   NOT NULL
 );
 
-CREATE TABLE "employees" (
-    "emp_no" int   NOT NULL,
-    "emp_title_id" varchar(10)   NOT NULL,
-    "birth_date" date   NOT NULL,
-    "first_name" varchar(40)   NOT NULL,
-    "last_name" varchar(40)   NOT NULL,
-    "sex" varchar(1)   NOT NULL,
-    "hire_date" date   NOT NULL,
-    CONSTRAINT "pk_employees" PRIMARY KEY (
-        "emp_no"
-     )
-);
-
-CREATE TABLE "salaries" (
-    "emp_no" int   NOT NULL,
-    "salary" int   NOT NULL
-);
-
-CREATE TABLE "titles" (
-    "title_id" varchar(10)   NOT NULL,
-    "title" varchar(40)   NOT NULL,
-    CONSTRAINT "pk_titles" PRIMARY KEY (
-        "title_id"
-     )
-);
 
 ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
